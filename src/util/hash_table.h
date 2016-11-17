1b340b1217fb77e55d62956759a6373591757b71
Julien Gautier <julien.gautier@epita.fr>
2016-11-17 22:52:06 +0100

    [DOXY] Added doxy to create_hash

--- src/util/hash_table.h
+++ src/util/hash_table.h
@@ -21,6 +21,11 @@ struct hash_table
   struct elt_hash **table;
 };
 
+/**
+ * \brief Create a hash_table
+ * \param capacity The number of element that trigger the increase in the hash
+ * table capacity
+*/ 
 struct hash_table *create_hash(size_t capacity);
 void destroy_hash(struct hash_table *ht);
 struct hash_table *add_hash(struct hash_table *ht, char *key, void *data);
