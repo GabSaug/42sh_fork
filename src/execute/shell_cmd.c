0138ca288983b02c3994eeaeb7a5726244bd1dee
Julien Gautier <julien.gautier@epita.fr>
2016-11-18 01:11:57 +0100

    [SHELL_CMD] Added execute_while

--- src/execute/shell_cmd.c
+++ src/execute/shell_cmd.c
@@ -24,3 +24,20 @@ int execute_if(struct tree *ast, struct hash_table *ht)
   else
     return 0;
 }
+
+int execute_while(struct tree *ast, struct hash_table *ht)
+{
+  struct tree *compound_list = v_get(ast->child, 1);
+  struct tree *cond = v_get(compound_list->child, 0);
+
+  while (execute_and_or(cond, ht) == 0)
+  {
+    compound_list = v_get(ast->child, 3);
+    struct tree *then = v_get(compound_list->child, 0);
+    return execute_and_or(then, ht);
+  }
+  else
+    return 0;
+}
+
+
