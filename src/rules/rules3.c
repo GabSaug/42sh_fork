08b6dddf55efd1a8b5671edc8425094e8d477e6b
Julien Gautier <julien.gautier@epita.fr>
2016-11-23 20:22:16 +0100

    [CLEAN_UP] Remove dead code

--- src/rules/rules3.c
+++ src/rules/rules3.c
@@ -72,30 +72,13 @@ static struct rule *init_rule_simple_command(void)
 {
   struct rule *rule = my_malloc(sizeof (struct rule));
   rule->nb_s_r = 1;
-  rule->s_r = my_malloc(sizeof (struct simple_rule) * 1); // inverse rules
+  rule->s_r = my_malloc(sizeof (struct simple_rule) * 1);
   // Rule 1
   rule->s_r[0].nb_sym = 1;
   rule->s_r[0].sym_arr = my_malloc(sizeof (struct symbol) * 1);
   create_sym(rule->s_r[0].sym_arr + 0, 0, PLUS, ELEMENT, 0);
 
-
   return rule;
-  
-  /*struct rule *rule = my_malloc(sizeof (struct rule));
-  rule->nb_s_r = 2;
-  rule->s_r = my_malloc(sizeof (struct simple_rule) * 2); // inverse rules
-  // Rule 1
-  rule->s_r[0].nb_sym = 2;
-  rule->s_r[0].sym_arr = my_malloc(sizeof (struct symbol) * 2);
-  create_sym(rule->s_r[0].sym_arr + 0, 0, STAR, PREFIX, 0);
-  create_sym(rule->s_r[0].sym_arr + 1, 0, PLUS, ELEMENT, 0);
-
-  // Rule 2
-  rule->s_r[1].nb_sym = 1;
-  rule->s_r[1].sym_arr = my_malloc(sizeof (struct symbol));
-  create_sym(rule->s_r[1].sym_arr, 0, PLUS, PREFIX, 0);
-
-  return rule;*/
 }
 
 static struct rule *init_rule_shell_command2(struct rule *rule)
