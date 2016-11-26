d9fac6985bbb0d6f923d42cf5d84f14f0bc78826
Arthur Remaud <remaud_a@epita.fr>
2016-11-26 01:22:57 +0100

    [DOC] doxygen filled for builtins functions

--- src/execute/builtins.h
+++ src/execute/builtins.h
@@ -13,10 +13,39 @@
 #include "main.h"
 #include "option_parser.h"
 
+/**
+ * \brief Execution of shopt builtin
+ * \param argv The arguments for the builtin
+ * \return The success or failure of the builtin
+*/
 int builtin_shopt(char *argv[]);
+
+/**
+ * \brief Execution of alias builtin
+ * \param argv The arguments for the builtin
+ * \return The success or failure of the builtin
+*/
 int builtin_alias(char *argv[]);
+
+/**
+ * \brief Execution of unalias builtin
+ * \param argv The arguments for the builtin
+ * \return The success or failure of the builtin
+*/
 int builtin_unalias(char *argv[]);
+
+/**
+ * \brief Execution of export builtin
+ * \param argv The arguments for the builtin
+ * \return The success or failure of the builtin
+*/
 int builtin_export(char *argv[]);
+
+/**
+ * \brief Execution of source builtin
+ * \param argv The arguments for the builtin
+ * \return The success or failure of the builtin
+*/
 int builtin_source(char *argv[]);
 
 #endif /* !BUILTINS_H */
