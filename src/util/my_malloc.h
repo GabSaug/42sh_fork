8cc378028cde6bd6bcd746d46f08e64c0df22e14
Arthur Remaud <remaud_a@epita.fr>
2016-11-27 04:21:53 +0100

    [DOXYGEN] doc for util added

--- src/util/my_malloc.h
+++ src/util/my_malloc.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 
+/**
+ * \brief Allocation of memory with error gestion
+ * \param size The size in bytes of memory to allocate
+ * \return The allocated pointer
+*/
 void *my_malloc(size_t size);
 
 #endif /* !MY_MALLOC_H */
