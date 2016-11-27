4bc8bca1e00a14f95a526998f3fd6b896285ba65
tourne_v <tourne_v@epita.fr>
2016-11-27 10:30:52 +0100

    [Coding Style] Coding style in my_string (more than 10 functions)

---
+++ src/util/my_string3.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+#include "my_malloc.h"
+#include "my_string.h"
+
+int begin_as(char* start, char* end, char s_list[][10])
+{
+  size_t len = end - start + 1;
+  for (int i = 0; s_list[i][0]; ++i)
+    if (!strncmp(start, s_list[i], len) && strlen(s_list[i]) >= len)
+      return i;
+  return -1;
+}
