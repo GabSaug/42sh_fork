d3e7e827ef943c54d42c3d54950eb5f43014d2d9
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-10-03 14:59:24 +0200

    Added single file compilation scripts

---
+++ compile_no_configure.sh
@@ -0,0 +1,11 @@
+opt="$(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g")"
+git restore src/util/vector2.c
+echo ./configure.sh "$opt -Wno-dev"
+./configure.sh "$opt -Wno-dev -finline-limit=2"
+cd build
+make clean
+make -Wno-dev -j -n > log_make.txt
+if ! make -j; then
+	echo "Error make"
+	exit 1
+fi
