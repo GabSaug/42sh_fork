a094397f7dc6430aea26091309809be69e9f7b39
SAUGER Gabriel <gabriel.sauger@loria.fr>
2025-04-09 15:59:09 +0200

    add no inlining option

--- compile_no_configure.sh
+++ compile_no_configure.sh
@@ -1,4 +1,4 @@
-opt="$(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g")"
+opt="-fno-inline $(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g")"
 git restore src/util/vector2.c
 echo ./configure.sh "$opt -Wno-dev"
 ./configure.sh "$opt -Wno-dev -finline-limit=2"
